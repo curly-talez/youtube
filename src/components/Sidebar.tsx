dc0acc9b9c167773993e83df1d01d1494c0c0461
Krishnaveni <krishnaveni.skps@gmail.com>
2025-06-20 13:39:58 +0200

    created sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,31 @@
 const Sidebar = () => {
-  return <h1> Side bar</h1>;
+  return (
+    <div className="shadow-lg p-5 shadow-gray-200 w-50">
+      <ul className="pt-5 ">
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Live</li>
+      </ul>
+
+      <h1 className="font-bold pt-5"> You</h1>
+      <ul>
+        <li>History</li>
+        <li>Playlists</li>
+        <li>Your videos</li>
+        <li>Watch later</li>
+        <li>Liked videos</li>
+      </ul>
+      <h1 className="font-bold pt-5">Explore</h1>
+      <ul>
+        <li>Trending</li>
+        <li>Music</li>
+        <li>Movies</li>
+        <li>News</li>
+        <li>Sports</li>
+        <li>Podcasts</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
